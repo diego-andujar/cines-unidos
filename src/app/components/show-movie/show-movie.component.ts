23950d68aa442048b73aa04e7b19d4c27732ec67
Javier <javierfuenmayor00@gmail.com>
2021-03-05 15:45:30 -0400

    se muestran las peliculas

--- src/app/components/show-movie/show-movie.component.ts
+++ src/app/components/show-movie/show-movie.component.ts
@@ -21,10 +21,10 @@ export class ShowMovieComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.getMovies();
+    this.getMovies(this.moviesList);
   }
 
-  getMovies(){
+  getMovies(listMovies){
     const value = "&query=furious";
     const url = "".concat(this.baseURL, "search/movie", this.APIKEY, value);
 
@@ -32,61 +32,23 @@ export class ShowMovieComponent implements OnInit {
       .then((res) => res.json())
       .then((data) => {
         const moviesToAdd = data.results;
-        this.createMovie(moviesToAdd);
-        console.log("Data: ", data);
+        this.createMovie(moviesToAdd, listMovies);
+        console.log(data)
       })
       .catch((error) => {
         console.log("error: ", error)
       })
-    console.log("value: ", value);
   }
 
-  getAllMovies(listMovies): Observable<Movie[]>{
-    return listMovies.map((changes) => {
-      return changes.map((movie) => ({
-        titulo: movie.original_title,
-        idiomas: movie.original_language,
-        imagen: movie.poster_path,
-        popularidad: movie.popularity,
-      }));
-    })
-  }
-
-  createMovie(listMovies){
+  createMovie(listMovies, movielist){
     return listMovies.map((movie) => {
       const actMovie: Movie = {
         titulo: movie.original_title,
         idiomas: movie.original_language,
-        imagen: movie.poster_path,
+        imagen: "https://image.tmdb.org/t/p/w500/" + movie.poster_path,
         popularidad: movie.popularity,
       }
-      this.moviesList.push(actMovie);
+      movielist.push(actMovie);
     })
   }
-
-  getConfig(){
-    const url = "".concat(this.baseURL, "configuration?api_key=", this.APIKEY)
-    fetch(url).then((result) => {
-      return result.json();
-    })
-    .then((data) => {
-      this.baseImageUrla = data.images.secure_base_url;
-      this.configData = data.images;
-      console.log("config:", data);
-      console.log("config fetched");
-      this.runSearch("up");
-    }).catch(function(err) {
-      alert(err);
-    })
-  }
-
-  runSearch(keyword: string) {
-    const url = "".concat(this.baseURL, "configuration?api_key=", this.APIKEY)
-    fetch(url).then(result =>
-      result.json()).then((data) => {
-        console.log( JSON.stringify(data, null, 4));
-        console.log(document)
-      })
-    
-  } 
 }
--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Movie } from '../models/movie';
 
 @Injectable({
   providedIn: 'root'
@@ -12,27 +13,32 @@ export class MoviesService {
 
   constructor() { }
 
-  getConfig(){
-    const url = "".concat(this.baseURL, "configuration?api_key=", this.APIKEY)
-    fetch(url).then((result) => {
-      return result.json();
-    })
-    .then((data) => {
-      this.baseImageUrla = data.images.secure_base_url;
-      this.configData = data.images;
-      console.log("config:", data);
-      console.log("config fetched");
-      this.runSearch("jaws");
-    }).catch(function(err) {
-      alert(err);
-    })
+  getMoviesByWord(listMovies){
+    const value = "&query=furious";
+    const url = "".concat(this.baseURL, "search/movie", this.APIKEY, value);
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        const moviesToAdd = data.results;
+        this.createMovieByWord(moviesToAdd, listMovies);
+        console.log(data)
+      })
+      .catch((error) => {
+        console.log("error: ", error)
+      })
   }
 
-  runSearch(keyword: string) {
-    const url = "".concat(this.baseURL, "configuration?api_key=", this.APIKEY)
-    fetch(url).then(result =>
-      result.json()).then((data) => {
-        document.getElementById("output").innerHTML = JSON.stringify(data, null, 4);
-      })
-  } 
+  createMovieByWord(listMovies, movielist){
+    return listMovies.map((movie) => {
+      const actMovie: Movie = {
+        titulo: movie.original_title,
+        idiomas: movie.original_language,
+        imagen: "https://image.tmdb.org/t/p/w500/" + movie.poster_path,
+        popularidad: movie.popularity,
+      }
+      movielist.push(actMovie);
+    })
+  }
+  
 }
