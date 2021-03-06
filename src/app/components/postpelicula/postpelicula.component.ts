ea0d1d7a9cf47036513b0ae73cdae307334f9745
Javier <javierfuenmayor00@gmail.com>
2021-03-05 22:59:19 -0400

    listo casi todo, falta el footer

--- src/app/components/postpelicula/postpelicula.component.ts
+++ src/app/components/postpelicula/postpelicula.component.ts
@@ -1,7 +1,8 @@
-import { MoviesService } from 'src/app/services/movies.service';
 import { Movie } from './../../models/movie';
 import { Component, Input, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import firebase from "firebase";
 
 @Component({
   selector: 'app-postpelicula',
@@ -10,19 +11,33 @@ import { Router } from '@angular/router';
 })
 export class PostpeliculaComponent implements OnInit {
 
+  userLiked = false;
+  user: firebase.User = null;
   @Input() extendedView = false;
   @Input() movie = {} as Movie;
   
   
   constructor(
     private router: Router,
+    private authService: AuthService,
     ) {}
 
   ngOnInit(): void {
+    this.authService.getCurrentUser().subscribe((user) => {
+      this.user = user;
+    })
   }
 
   shareMovie(movie: Movie) {
-    this.extendedView = true;
+    this.extendedView = !this.extendedView;
+  }
+
+  favClick(){
+    this.userLiked = true;
+  }
+
+  unfavClick(){
+    this.userLiked = false;
   }
 
 }
